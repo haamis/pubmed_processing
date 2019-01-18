87b7ddf5db2793aa0def2d1e5887bdb4cb248611
Antti Virtanen <haama92@gmail.com>
2019-01-18 19:29:42 +0200

    Make output json pretty.

--- split_json_data.py
+++ split_json_data.py
@@ -14,4 +14,4 @@ random.shuffle(data)
 print("Dumping..")
 
 with open("./" + sys.argv[2], "wt") as f:
-    json.dump(data[0:len(data)//10], f)
+    json.dump(data[0:len(data)//10], f, indent=2, sort_keys=True)
