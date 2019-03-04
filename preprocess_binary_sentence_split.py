a7a07f4122808d1f2ea01b06406bafa02e293b28
Antti Virtanen <haama92@gmail.com>
2019-03-04 19:40:02 +0200

    Experimenting with splitting abstracts to sentences.

---
+++ preprocess_binary_sentence_split.py
@@ -0,0 +1,42 @@
+import json, re, sys, os, pickle
+from tqdm import tqdm
+from nltk import tokenize
+
+def preprocess_data(input_file, mesh_file):
+    print("Reading input files..")
+
+    with open(input_file) as f:
+        data = json.load(f)
+
+    print("Reading mesh data..")
+
+    with open(mesh_file) as f:
+        mesh_ids = set(json.load(f))
+
+    reg_string = r"|".join([x for x in mesh_ids])
+
+    reg = re.compile(reg_string)
+
+    abstracts = []
+    is_neuro = []
+
+    for article in tqdm(data, desc="Grabbing abstracts and mesh terms"):
+        abstract = "\n".join([x["text"] for x in article["abstract"]])
+        for sentence in tokenize.sent_tokenize(abstract):
+            abstracts.append(sentence)
+            is_neuro.append(0)
+            for mesh_term in article["mesh_list"]:
+                if reg.match(mesh_term["mesh_id"]):
+                    is_neuro[-1] = 1
+                    break
+
+    print("Dumping..")
+
+    with open(os.path.splitext(input_file)[0] + "_sentence_abstracts.dump", "wb") as f:
+        pickle.dump(abstracts, f)
+
+    with open(os.path.splitext(input_file)[0] + "_sentence_is_neuro.dump", "wb") as f:
+        pickle.dump(is_neuro, f)
+
+if __name__ == '__main__':
+    preprocess_data(sys.argv[1], sys.argv[2])
